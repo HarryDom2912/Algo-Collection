5593b1b36b64ded737847fa38eda17b299110813
nguyenhung2912200 <nguyenhung202912@gmail.com>
2023-11-30 15:10:01 +0700

    V3

--- GCD_UCLN_BCNN/Loop/GCD_by_Loop.cpp
+++ GCD_UCLN_BCNN/Loop/GCD_by_Loop.cpp
@@ -1,5 +1,35 @@
+/*  _____                                                                             _____  */
+/* ( ___ )                                                                           ( ___ ) */
+/*  |   |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|   |  */
+/*  |   |                                                                             |   |  */
+/*  |   |                                                                             |   |  */
+/*  |   |    _   _                       ____                  ____   ___  _ ____     |   |  */
+/*  |   |   | | | | __ _ _ __ _ __ _   _|  _ \  ___  _ __ ___ |___ \ / _ \/ |___ \    |   |  */
+/*  |   |   | |_| |/ _` | '__| '__| | | | | | |/ _ \| '_ ` _ \  __) | (_) | | __) |   |   |  */
+/*  |   |   |  _  | (_| | |  | |  | |_| | |_| | (_) | | | | | |/ __/ \__, | |/ __/    |   |  */
+/*  |   |   |_| |_|\__,_|_|  |_|   \__, |____/ \___/|_| |_| |_|_____|  /_/|_|_____|   |   |  */
+/*  |   |                          |___/                                              |   |  */
+/*  |   |                                                                             |   |  */
+/*  |   |                                                                             |   |  */
+/*  |___|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|___|  */
+/* (_____)                                                                           (_____) */
+
 // Source: Series: "Algo-Zero" - Code Mely - Dev ơi mình đi đâu thế ?
 // Tìm ước chung lớn nhất (UCLN) & Bội chung nhỏ nhất bằng Vòng lặp 
+
+/*------Ý tưởng-----
+1/ Tìm UCLN
+B1: Tìm số nhỏ hơn giữa 2 số a và b
+B2: Duyệt vòng lặp với chỉ số i giảm dần từ min(a,b) tới 1 ( tính cả số 1 và min(a,b))
+B3: Nếu cả 2 số a, b đều chia hết cho i thì dừng vòng lặp, i chính là UCLN cần tìm
+2/ Tìm BCNN
+Sau khi đã có UCLN, có thể dùng công thức a*b=UCLN*BCNN
+*/
+
+/*------Pros and cons------
+Pros: Đơn giản, dễ tiếp cận đối với người mới. Dung lượng lưu trữ code nhỏ
+Cons: code dài dòng, Độ phức tạp của thuật toán chưa tối ưu O(min(a,b)) 
+*/
 
 #include <iostream>
 
@@ -21,7 +51,7 @@ int main(){
     std::cin >> b;
 
     i = a > b ? b : a;
-    // Gán cho i giá trị nhỏ hơn trong 2 số a và b
+    // Gán cho i giá trị nhỏ hơn giữa a và b
     std::cout << "UCLN = " << GCD(a, b, i) << std::endl;
     std::cout << "BCNN = " << a*b/GCD(a, b, i) << std::endl;
 
